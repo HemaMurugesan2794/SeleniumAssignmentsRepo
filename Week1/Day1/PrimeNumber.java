7696a78b5ed004daaf4e2c819e34094efd13370b
HemaMurugesan2794 <89142030+HemaMurugesan2794@users.noreply.github.com>
2022-12-09 14:58:25 +0530

    Add files via upload
    Week1.day1 Assignment

---
+++ Week1/Day1/PrimeNumber.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		
+		int n = 19;
+		int count=0;
+		for (int i = 1; i<=n ;i++) {
+			
+			if (n%i==0) 
+			count++;
+			
+		}
+		if(count==2)
+		{
+			System.out.println(n +" is a Prime Number");
+		}
+		else
+		{
+			System.out.println(n +" is not a Prime Number");
+			
+		}
+
+	
+	}
+
+}
